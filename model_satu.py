be72a51160b5e992b7742d4e4404591a39de38dc
nabilaalifm <148742310+nabilaalifm@users.noreply.github.com>
2025-05-03 19:54:52 +0700

    Add files via upload

---
+++ model_satu.py
@@ -0,0 +1,7 @@
+import pickle
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("Halo, Streamlit!")
+st.write("Ini adalah aplikasi web interaktif pertama kamu dengan Streamlit.")
